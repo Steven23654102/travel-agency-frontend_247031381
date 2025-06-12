2c7458800aaa1ec4e60cf775e2d505e158eb1596
Steven23654102 <apple23654102@gmail.com>
2025-06-13 00:27:00 +0800

    modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppointmentForm from "./pages/AppointmentForm";
-import AppointmentList from "./pages/AppointmentList";    // ★ 新增
+import AppointmentList from "./pages/AppointmentList";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<AppointmentForm />} />
-        <Route path="/appointments" element={<AppointmentList />} />  {/* ★ */}
+        <Route path="/appointments" element={<AppointmentList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
