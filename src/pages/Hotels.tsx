7f8471c892e2aef6de0b7c14421e72f60b0a12f4
Steven23654102 <apple23654102@gmail.com>
2025-06-13 01:18:20 +0800

    create Hotels.tsx

---
+++ src/pages/Hotels.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getHotels } from "../api/hotels";
+
+interface Hotel {
+  id: number;
+  name: string;
+  city: string;
+  description: string;
+}
+
+export default function Hotels() {
+  const [hotels, setHotels] = useState<Hotel[]>([]);
+
+  useEffect(() => {
+    getHotels().then(setHotels);
+  }, []);
+
+  return (
+    <div>
+      <h2>所有飯店</h2>
+      <ul>
+        {hotels.map(hotel => (
+          <li key={hotel.id}>
+            <Link to={`/hotels/${hotel.id}`}>{hotel.name}</Link> - {hotel.city}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
